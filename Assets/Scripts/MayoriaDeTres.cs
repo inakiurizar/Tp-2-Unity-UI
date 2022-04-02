a8dd2c7a57d037f459885ed24d4607b772193085
inakiurizar <101410794+inakiurizar@users.noreply.github.com>
2022-04-02 16:09:46 -0300

    Completed Unity UI exercise

---
+++ Assets/Scripts/MayoriaDeTres.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeTres : MonoBehaviour
+{
+    public int edadUsuario;
+    public Text miTexto;
+
+    void Start()
+    {
+        if(edadUsuario >= 18)
+        {
+            miTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            miTexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseñaCorrecta;
+    string contrasenaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+    void Start()
+    {
+        contraseñaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void validarContraseña()
+    {
+        contrasenaUsuario = ingresoUsuario.text;
+        if (contrasenaUsuario == contraseñaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña incorrecta";
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
